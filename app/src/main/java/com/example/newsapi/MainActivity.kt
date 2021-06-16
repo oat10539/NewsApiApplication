041d3f8929bbd77e2de6e3613790a1ccfbc249d7
oat10539 <60010685@kmitl.ac.th>
2021-06-16 23:02:18 +0700

    Init viewmodel and observe data from viewmodel

--- app/src/main/java/com/example/newsapi/MainActivity.kt
+++ app/src/main/java/com/example/newsapi/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.newsapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsapi.viewmodel.ViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: ViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.getNews()
+
+        viewModel.errorMessage().observe(this, { error ->
+        })
+        viewModel.newsList().observe(this, { newsList ->
+        })
     }
 }
