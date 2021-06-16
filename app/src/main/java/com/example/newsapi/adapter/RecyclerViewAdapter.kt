4085228248cc2d012b55558c0b8c859df0af0ddf
oat10539 <60010685@kmitl.ac.th>
2021-06-17 00:29:02 +0700

    Add onclick listener go to news detail activity

--- app/src/main/java/com/example/newsapi/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/newsapi/adapter/RecyclerViewAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.newsapi.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.newsapi.NewsDetailsActivity
 import com.example.newsapi.databinding.ItemRecyclerBinding
 import com.example.newsapi.model.NewsDetails
 
@@ -16,6 +19,13 @@ class RecyclerViewAdapter(private val newsArrayList: List<NewsDetails>) :
         fun bind(item: NewsDetails) {
             binding.newsDetail = item
             binding.executePendingBindings()
+        }
+        init {
+            binding.root.setOnClickListener {
+                val intent = Intent(it.context, NewsDetailsActivity::class.java)
+                    .putExtra("news",binding.newsDetail)
+                ContextCompat.startActivity(it.context, intent, null)
+            }
         }
     }
 
