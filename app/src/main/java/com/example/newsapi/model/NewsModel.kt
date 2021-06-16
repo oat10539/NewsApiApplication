83af2e87dd6279a08b5434b94ebf304496e326c7
oat10539 <60010685@kmitl.ac.th>
2021-06-17 00:28:02 +0700

    Add @Parcelize to data class

--- app/src/main/java/com/example/newsapi/model/NewsModel.kt
+++ app/src/main/java/com/example/newsapi/model/NewsModel.kt
@@ -1,11 +1,15 @@
 package com.example.newsapi.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 
 data class NewsModel (
     var status: String? = null,
     var articles: List<NewsDetails>? = null
 )
 
+@Parcelize
 data class NewsDetails (
     var author: String? = null,
     var title: String? = null,
@@ -14,4 +18,4 @@ data class NewsDetails (
     var urlToImage: String? = null,
     var publishedAt: String? = null,
     var content: String? = null
-)
+): Parcelable
