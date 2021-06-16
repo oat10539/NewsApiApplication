eadd97c0264f7dbbf6e1b4f412965f1e1cfe387d
oat10539 <60010685@kmitl.ac.th>
2021-06-17 00:26:51 +0700

    Create news details activity

---
+++ app/src/main/java/com/example/newsapi/NewsDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newsapi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewsDetailsActivity : AppCompatActivity() {
+
+    override  fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.news_details)
+    }
+}
