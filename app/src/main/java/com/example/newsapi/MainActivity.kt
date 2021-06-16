1c558bc653ccfc032cf51b5b24dfbdc6e53fabfb
oat10539 <60010685@kmitl.ac.th>
2021-06-16 23:27:34 +0700

    Create recycler view and binding MainActivity with xml

--- app/src/main/java/com/example/newsapi/MainActivity.kt
+++ app/src/main/java/com/example/newsapi/MainActivity.kt
@@ -2,17 +2,19 @@ package com.example.newsapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.newsapi.databinding.ActivityMainBinding
 import com.example.newsapi.viewmodel.ViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: ViewModel by viewModel()
+    private lateinit var  binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel.getNews()
 
         viewModel.errorMessage().observe(this, { error ->
