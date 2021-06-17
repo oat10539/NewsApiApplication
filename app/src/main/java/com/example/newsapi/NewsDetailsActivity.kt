afa228fe3a7ea3af456095a6f0378ea7477b1df0
oat10539 <60010685@kmitl.ac.th>
2021-06-17 21:23:45 +0700

    View and data binding in activity
    -get data from previous page and binding to show news details

--- app/src/main/java/com/example/newsapi/NewsDetailsActivity.kt
+++ app/src/main/java/com/example/newsapi/NewsDetailsActivity.kt
@@ -2,11 +2,19 @@ package com.example.newsapi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.newsapi.databinding.NewsDetailsBinding
+import com.example.newsapi.model.NewsDetails
 
 class NewsDetailsActivity : AppCompatActivity() {
+    private lateinit var binding : NewsDetailsBinding
 
     override  fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.news_details)
+         binding = DataBindingUtil.setContentView(this, R.layout.news_details)
+
+        val news: NewsDetails? = intent.extras!!.getParcelable("news")
+        binding.newsDetail = news
+
     }
 }
